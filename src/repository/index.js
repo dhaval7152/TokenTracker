18e0585a72563925b83af927e8af4670c5097087
Dhaval saxena <dhaval.saxena@minddeft.net>
2023-09-15 18:57:51 +0530

    unstructure but working

--- src/repository/index.js
+++ src/repository/index.js
@@ -2,23 +2,23 @@ const ethers = require("ethers");
 require("dotenv").config();
 
 const provider = new ethers.providers.JsonRpcProvider(
-  process.env.sepolia_network
+  "https://sepolia.infura.io/v3/4ec1f52fc49f48fb9f8bac27b1348a42"
 );
-const listenToTransactions = async (_address) => {
+const listenToTransactions = async () => {
   const block = await provider.getBlock(4293509);
   const result = await provider.getBlockWithTransactions(block.hash);
   const allTransactions = result.transactions;
   for (let i = 0; i < allTransactions.length; i++) {
     if (
-      allTransactions[i].from === "0x0fadb24c9a7ac088c329c4fa87730d3b2df2f525"
+      allTransactions[i].from === '0x0fadb24C9A7ac088c329C4Fa87730D3B2df2f525'
     ) {
       console.log(allTransactions[i]);
-      console.log("first");
     }
-    console.log("in else");
+    // console.log("in else");
   }
 };
+listenToTransactions()
 
-listenToTransactions("0x0fadb24c9a7ac088c329c4fa87730d3b2df2f525");
+// listenToTransactions("0x0fadb24c9a7ac088c329c4fa87730d3b2df2f525");
 
 // module.exports = { listenToTransactions };
