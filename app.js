fe19e6eab7a0e712ff75827b300d081382f39877
Dhaval saxena <dhaval.saxena@minddeft.net>
2023-09-15 11:34:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const requestIp=require('request-ip');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/ip', (req, res)=> { 
+    const clientIp = requestIp.getClientIp(req);
+    console.log(clientIp)
+    res.send({ ip : clientIp})
+})
+app.get('/err', (req, res)=> { 
+  throw new Error('BROKEN')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
