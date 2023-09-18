b0f518ffa4cbb56aec20ceb7463f2dd75ae6cbfa
= <=>
2023-09-18 15:27:50 +0530

    structured

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
 const express = require("express");
-const mainRoute = require("../Routes/MainRoute");
+const mainRoute = require("./Routes");
 const cors = require("cors");
-const { FetchTransactionDetail } = require("./repository");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.port || 2000;
 
@@ -12,12 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes imported
 app.use(mainRoute);
-
-app.get("/getToken/:account", async (req, res) => {
-  let account = req.params.account;
-  FetchTransactionDetail(account);
-  res.send("listening to block");
-});
 
 app.listen(port, () => {
   console.log(`Server is Running On http://localhost:${port}`);
